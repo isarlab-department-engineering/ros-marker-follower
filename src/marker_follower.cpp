12c8d127d6a227be38081e87ac831bbdcbbe6fe5
Andrea Fioroni <andrifiore@gmail.com>
2017-05-21 17:07:43 +0200

    added file launch for the entire following (all nodes needed) also deleted some comments

--- src/marker_follower.cpp
+++ src/marker_follower.cpp
@@ -31,7 +31,7 @@ int startTime=0;
 float estimatedMotorZeroPower = 0;
 float estimatedMotorOnePower = 0;
   
-float distanceTarget=0.3; //target distance in cm
+float distanceTarget=0.5; //target distance in cm
 const float ANGLETARGET=0; //target angle in cm. NOTE: since the target is 0.0, angle itself is the angle error signal 
 const float DISTANCEPIDPROPORTIONALCONSTANT=1000;
 float anglePIDProportionalConstant;
@@ -143,8 +143,8 @@ int main(int argc, char **argv) {
 	ros::NodeHandle n;   
 	ros::Rate loop_rate(10);   
 	
-	if(argv[1]!=NULL)
-		distanceTarget=strtof((argv[1]),0);
+	//if(argv[1]!=NULL)
+	//	distanceTarget=strtof((argv[1]),0);
 	
 	// SUBSCRIBER   
 	ros::Subscriber sub = n.subscribe("markers_stream", 10, markerCallback);   
@@ -171,100 +171,4 @@ int main(int argc, char **argv) {
 			motorZeroPower=motorZeroPower-(angle-ANGLETARGET)*ANGLEPIDPROPORTIONALCONSTANT;
 			motorOnePower=motorOnePower+(angle-ANGLETARGET)*ANGLEPIDPROPORTIONALCONSTANT;
 		}
-*/
-
-/*
-#include "ros/ros.h"
-#include "ros/package.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Int16MultiArray.h"
-#include "aruco_detection/ArMarkers.h"
-#include <sstream>
-#include <math.h>
-
-// OPENCV
-//#include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-// ARUCO
-//#include <opencv2/aruco.hpp>
-
-using namespace std;
-//using namespace cv;
-
-// ros basic elements
-ros::Publisher pub;
-vector<double> r, t;
-vector<int> ids; 
-int markerNo;
-float d;
-float angle;
-
-
-void markerCallback(aruco_detection::ArMarkers msg) {
-	markerNo=msg.markerNo;   
-	r=msg.rVecs;   
-	t=msg.tVecs;   
-	ids=msg.markersIds;
-  
-  ros::Rate loop_rate(10);
-
-  for(int i=0; i<t.size(); i+=3){
-    d = sqrt( pow(t.at(i),2) + pow(t.at(i+1),2) + pow(t.at(i+2),2));
-    angle = atan(t.at(i)/t.at(i+2));
-    
-    ROS_INFO("distance of %i : %f", ids.at((int)i/3), d);
-    ROS_INFO("angle of %i : %f", ids.at((int)i/3), angle);
-
-  }
-
-  //da perfezionare chiaramente. diciamo che lo voglio mantenere a 30 cm in questa prima istanza
-  if(markerNo==1){
-	  std_msgs::Int16MultiArray motorSpeed;
-	  if(d>0.30){
-	  	motorSpeed.data.push_back(200);
-		motorSpeed.data.push_back(180);
-	  }
-	  else{
-	        motorSpeed.data.push_back(0);
-		motorSpeed.data.push_back(0);
-	  }
-	  pub.publish(motorSpeed);
-  }
-  else{
-	  std_msgs::Int16MultiArray motorSpeed;
-	  motorSpeed.data.push_back(0);
-	  motorSpeed.data.push_back(0);
-	  
-	  pub.publish(motorSpeed);
-  }
-  ros::spinOnce();
-
-  loop_rate.sleep();
-}
-
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "marker_follower");
-  ros::NodeHandle n;
-  ros::Rate loop_rate(1);
-
-  //namedWindow( "Output window", WINDOW_AUTOSIZE );
-
-  // SUBSCRIBER
-  ros::Subscriber sub = n.subscribe("markers_stream", 5000, markerCallback);
-
-  // PUBLISHER
-  pub = n.advertise<std_msgs::Int16MultiArray>("cmd", 1000);
-
-  while (ros::ok())
-  {
-    ros::spinOnce();
-
-    loop_rate.sleep();
-  }
-
-
-  return 0;
-}
 */
